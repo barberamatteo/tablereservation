cc1adb3e73efc6b9f04cc748aa50399596b5f5ca
matteo <m.barbera13@campus.unimib.it>
2025-03-04 23:52:10 +0100

    Fixed visualization of tables and reservation (currently working for a single one)

--- fe/src/components/LoungeCardHolder.tsx
+++ fe/src/components/LoungeCardHolder.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import Table from "../model/Table.ts"
 import Reservation from "../model/Reservation.ts"
 import TableCard from "./TableCard.tsx";
-import reservation from "../model/Reservation.ts";
+
 async function fetchTables(): Promise<Table[]> {
     const response = await fetch('http://localhost:8080/api/v1.0.0/admin/tables/getall/');
     return await response.json();
@@ -10,47 +10,47 @@ async function fetchTables(): Promise<Table[]> {
 
 async function fetchReservations(): Promise<Map<string, Reservation[]>>{
     const response = await fetch('http://localhost:8080/api/v1.0.0/user/reservation/getall/');
-    return await response.json();
+    const data = await response.json();
+    return new Map(Object.entries(data));
 }
 
+function rawToEffectiveMap(map: Map<string, Reservation[]>): Map<Table, Reservation[]> {
+    const toRet = new Map<Table, Reservation[]>();
+    map.forEach((v, k) => {
+        toRet.set(JSON.parse(k) as Table, v);
+    });
+    return toRet;
+}
 function LoungeCardHolder() {
     const [tables, setTables] = useState<Table[]>([]);
-    const [reservations, setReservations] = useState<Map<string, Reservation[]>>(new Map());
-
-
+    const [reservationsRaw, setReservationsRaw] = useState<Map<string, Reservation[]>>(new Map());
+    const [reservations, setReservations] = useState<Map<Table, Reservation[]>>(new Map());
     useEffect(() => {
-        async function loadTables() {
+        async function loadData(){
             try {
-                const data = await fetchTables();
-                setTables(data);
-            } catch (error) {
-                console.error('Error occurred loading tables:', error);
+                const tablesData = await fetchTables();
+                setTables(tablesData);
+
+                const reservationData = await fetchReservations();
+                setReservationsRaw(reservationData);
+
+                setReservations(rawToEffectiveMap(reservationData));
+
+            } catch{
+                console.log("Error during fetch of tables and reservationsRaw.");
             }
         }
-        loadTables().then(() => console.log(tables));
+        loadData().then();
 
-        async function loadReservations(){
-            try {
-                const data = await fetchReservations();
-                setReservations(data);
-            } catch (error){
-                console.error('Error occurred loading reservations:', error);
-            }
-        }
-        loadReservations().then();
-
-    }, [tables]);
+    }, []);
 
     return (
         <>
             <div className="container" style={{marginTop: "10rem"}}>
                 <div className="row gap-5">
-                    {/*
-
-                        Array.from(reservations.entries()).map((row, index) => (
-                        <TableCard table={JSON.parse(row[0]) as Table} reservations={row[1]}/>
-                    ))
-                    */}
+                    {Array.from(reservations.entries()).map(([key, value]) => (
+                        <TableCard table={key} reservations={value} />
+                    ))}
                 </div>
             </div>
         </>
